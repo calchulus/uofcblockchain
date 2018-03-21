32f5b7c9e043c5c1bc61a6af1c77da0b34613bb1
Calvin Chu <calc@uchicago.edu>
2018-03-21 17:48:35 -0400

    Add files via upload

---
+++ src/app/admin/shared/site-config/site-config.ts
@@ -0,0 +1,28 @@
+export class SiteConfig {
+  groupName: string;
+  groupWebsite: string;
+
+  googleLink: string;
+  facebookLink: string;
+  twitterLink: string;
+  meetupLink: string;
+  githubLink: string;
+
+  eventName: string;
+  eventDate: string;
+  eventEmail: string;
+  eventLink: string;
+  eventAnonReport: string;
+  eventReportEmail: string;
+  eventTwitter: string;
+  eventHashtag: string;
+  speakerURL: string;
+  ticketURL: string;
+  sponsorURL: string;
+
+  venueName: string;
+  venueAddress: string;
+  venueGeoData: any;
+  venueDescription: string;
+  venuePhoto: File;
+}
